05075d22834f105b692554a400d67304d1e9a0cf
kolazojar <mkrolbass@gmail.com>
2020-12-26 18:12:11 -0500

    removed memory and disk usage

--- dwm-simple-status.c
+++ dwm-simple-status.c
@@ -4,12 +4,6 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
-#include <sys/sysinfo.h>
-#include <sys/statvfs.h>
-#ifdef LAPTOP
-#include <signal.h>
-#endif
-
 #include <X11/Xlib.h>
 
 #define INTERVAL 10
@@ -19,12 +13,9 @@
 #define LBUF_SIZE (1 << 8)
 #define TEMP_DIR "/sys/devices/platform/coretemp.0/hwmon"
 #define TEMP_GUESS "/sys/class/hwmon/hwmon0/temp1_input"
-
-#ifdef LAPTOP
+#ifdef BATTERY
 #define BAT_DIR "/sys/class/power_supply/BAT0/"
 #define BAT_FILE(NAME) BAT_DIR NAME
-#define BACKLIGHT_DIR "/sys/class/backlight/intel_backlight/"
-#define BACKLIGHT_FILE(NAME) BACKLIGHT_DIR NAME
 #endif
 
 void read_file(char *path, char *line, size_t size)
@@ -90,30 +81,6 @@ void get_temp(char *str, size_t size, char *temp_file)
     }
 }
 
-void get_mem(char *str, size_t size)
-{
-    struct sysinfo info;
-
-    if (!sysinfo(&info)) {
-        double used = 100.0*(1.0 - ((double)info.freeram)/((double)info.totalram));
-        snprintf(str, size, "%.0f%%", used);
-    } else {
-        *str = '\0';
-    }
-}
-
-void get_disk(char *str, size_t size, char *path)
-{
-    struct statvfs stats;
-
-    if (!statvfs(path, &stats)) {
-        double used = 100.0*(1.0 - ((double)stats.f_bfree)/((double)stats.f_blocks));
-        snprintf(str, size, "%.0f%%", used);
-    } else {
-        *str = '\0';
-    }
-}
-
 void get_datetime(char *str, size_t size)
 {
     time_t now = time(NULL);
@@ -123,7 +90,7 @@ void get_datetime(char *str, size_t size)
     }
 }
 
-#ifdef LAPTOP
+#ifdef BATTERY
 void get_battery(char *str, size_t size)
 {
     char capacity[SBUF_SIZE];
@@ -150,26 +117,6 @@ void get_battery(char *str, size_t size)
         snprintf(str, size, "%c%s%%", sym, capacity);
     }
 }
-
-void get_backlight(char *str, size_t size)
-{
-    char current[SBUF_SIZE];
-    char max[SBUF_SIZE];
-
-    read_file(BACKLIGHT_FILE("actual_brightness"), current, SBUF_SIZE);
-    read_file(BACKLIGHT_FILE("max_brightness"), max, SBUF_SIZE);
-
-    if (current[0] == '\0' || max[0] == '\0') {
-        *str = '\0';
-    } else {
-        snprintf(str, size, "%.0f%%", atof(current)/atof(max)*100);
-    }
-}
-
-void handler(int sig)
-{
-    signal(SIGUSR1, handler);
-}
 #endif
 
 int main(void)
@@ -185,36 +132,23 @@ int main(void)
     get_temp_file(temp_file, MBUF_SIZE);
 
     char status[LBUF_SIZE];
+
     char datetime[MBUF_SIZE];
     char temp[SBUF_SIZE];
-    char mem[SBUF_SIZE];
-    char disk1[SBUF_SIZE];
-#ifndef LAPTOP
-    char disk2[SBUF_SIZE];
-#endif
-#ifdef LAPTOP
-    char backlight[SBUF_SIZE];
+#ifdef BATTERY
     char battery[SBUF_SIZE];
-
-    signal(SIGUSR1, handler);
 #endif
 
     for (;;sleep(INTERVAL)) {
-#ifdef LAPTOP
+#ifdef BATTERY
         get_datetime(datetime, MBUF_SIZE);
         get_temp(temp, SBUF_SIZE, temp_file);
         get_battery(battery, SBUF_SIZE);
-        get_backlight(backlight, SBUF_SIZE);
-        get_mem(mem, SBUF_SIZE);
-        get_disk(disk1, SBUF_SIZE, "/");
-        snprintf(status, LBUF_SIZE, " Brig %s | Disk %s | Mem %s | Temp %s | Bat %s | %s", backlight, disk1, mem, temp, battery, datetime);
+        snprintf(status, LBUF_SIZE, " %s | %s | %s", temp, battery, datetime);
 #else
         get_datetime(datetime, MBUF_SIZE);
         get_temp(temp, SBUF_SIZE, temp_file);
-        get_mem(mem, SBUF_SIZE);
-        get_disk(disk1, SBUF_SIZE, "/");
-        get_disk(disk2, SBUF_SIZE, "/home");
-        snprintf(status, LBUF_SIZE, " Disk / %s /home %s | Mem %s | Temp %s | %s", disk1, disk2, mem, temp, datetime);
+        snprintf(status, LBUF_SIZE, " %s | %s", temp, datetime);
 #endif
         XStoreName(dpy, DefaultRootWindow(dpy), status);
         XSync(dpy, 0);
